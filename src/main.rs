e616652ed2989cca676c90efb97cfc1c6b1153da
Salih Bozkaya <salihbozkaya@Salihs-MacBook-Pro.local>
2023-08-17 10:43:31 +0300

    add lexer

--- src/main.rs
+++ src/main.rs
@@ -14,12 +14,65 @@ fn main() {
 
     for entry in dir {
         let path = entry.expect("TODO:").path();
-        let content = read_entire_file(&path).unwrap_or_else(|err| {
-            eprintln!("{}", err);
-            exit(1);
-        });
+        let content = read_entire_file(&path)
+            .unwrap_or_else(|err| {
+                eprintln!("{}", err);
+                exit(1);
+            })
+            .chars()
+            .collect::<Vec<_>>();
 
-        println!("length of {:?} => {:?}", path, content.len());
+        let lexer = Lexer::new(&content);
+
+        for token in lexer {
+            println!("token {:?}", token);
+        }
+
+        // println!("lexers all chars {:?} ", lexer.content);
+        break;
+    }
+}
+
+struct Lexer<'a> {
+    content: &'a [char],
+}
+
+impl<'a> Lexer<'a> {
+    fn new(content: &'a [char]) -> Self {
+        return Self { content };
+    }
+
+    fn trim_left(&mut self) {
+        while !self.content.is_empty() && self.content[0].is_whitespace() {
+            self.content = &self.content[1..];
+        }
+    }
+
+    fn next_token(&mut self) -> Option<String> {
+        self.trim_left();
+
+        if self.content.len() == 0 {
+            return None;
+        }
+
+        let mut n = 0;
+
+        while self.content[0].is_alphabetic() && self.content[n].is_alphanumeric() {
+            n += 1;
+        }
+
+        let token = &self.content[0..n];
+        self.content = &self.content[n + 1..];
+
+        Some(token.into_iter().collect::<String>())
+    }
+}
+// fn next(&mut self) -> Option<Self::Item>
+
+impl<'a> Iterator for Lexer<'a> {
+    type Item = String;
+    fn next(&mut self) -> Option<<Self as Iterator>::Item> {
+        self.next_token()
     }
 }
 
