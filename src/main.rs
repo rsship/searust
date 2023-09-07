3ae1ae5ebd9c2c4c0bb95a407d0b90278148ac25
darth_vader <bozkayasalih01x@gmail.com>
2023-09-08 00:06:51 +0300

    Devel (#3)
    * feat: developed util mod for io operatons
    
    * feat: seperated Lexer mod
    
    * chore: extended .gitignore
    
    * cleanup: extended error type and clean up main.rs
    
    ---------
    
    Co-authored-by: simon hancker <142432057+seekingUniverse@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 mod config;
+mod lexer;
+mod util;
 
-use std::collections::HashMap;
+use lexer::*;
 use std::env;
-use std::error::Error;
-use std::fs::{self, File};
-use std::path::Path;
+use std::fs;
 use std::process::exit;
-use xml::reader::{EventReader, XmlEvent};
+use util::utils::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -20,9 +20,10 @@ fn main() {
         exit(1);
     });
 
-    let mut collection: HashMap<String, usize> = HashMap::new();
+    let mut tf_index = TermFreqIndex::new();
 
     for entry in dir {
+        let mut tf = TermFreq::new();
         let path = entry.expect("TODO:").path();
         let content = read_entire_file(&path)
             .unwrap_or_else(|err| {
@@ -32,94 +33,32 @@ fn main() {
             .chars()
             .collect::<Vec<_>>();
 
-        println!("-> {:?}", path);
+        println!("Indexing {}... ", path.display());
+
         for token in Lexer::new(&content) {
             let token = token
                 .into_iter()
-                .map(|x| x.to_ascii_uppercase())
+                .map(|x| x.to_ascii_lowercase())
                 .collect::<String>();
 
-            if let Some(count) = collection.get_mut(&token) {
+            if let Some(count) = tf.get_mut(&token) {
                 *count += 1;
             } else {
-                collection.insert(token, 1);
+                tf.insert(token, 1);
             }
         }
 
-        println!("collection => {:?}", collection.into_iter().take(1));
-        break;
-    }
-}
+        let mut stats = tf.iter().collect::<Vec<_>>();
+        stats.sort_by_key(|(_, f)| *f);
+        stats.reverse();
 
-struct Lexer<'a> {
-    content: &'a [char],
-}
-
-impl<'a> Lexer<'a> {
-    fn new(content: &'a [char]) -> Self {
-        return Self { content };
+        tf_index.insert(path, tf);
     }
 
-    fn trim_left(&mut self) {
-        while !self.content.is_empty() && self.content[0].is_whitespace() {
-            self.content = &self.content[1..];
-        }
-    }
+    println!("Indexing Done...");
 
-    fn chop(&mut self, idx: usize) -> &'a [char] {
-        let token = &self.content[0..idx];
-        self.content = &self.content[idx..];
-        return token;
-    }
-
-    fn chop_while<P>(&mut self, mut predicate: P) -> &'a [char]
-    where
-        P: FnMut(&char) -> bool,
-    {
-        let mut n = 0;
-        while n < self.content.len() && predicate(&self.content[n]) {
-            n += 1;
-        }
-
-        return self.chop(n);
-    }
-
-    fn next_token(&mut self) -> Option<&'a [char]> {
-        self.trim_left();
-
-        if self.content.len() == 0 {
-            return None;
-        }
-
-        if self.content[0].is_numeric() {
-            return Some(self.chop_while(|x| x.is_numeric()));
-        };
-
-        if self.content[0].is_alphabetic() {
-            return Some(self.chop_while(|x| x.is_alphabetic()));
-        };
-
-        return Some(self.chop(1));
-    }
-}
-// fn next(&mut self) -> Option<Self::Item>
-
-impl<'a> Iterator for Lexer<'a> {
-    type Item = &'a [char];
-    fn next(&mut self) -> Option<<Self as Iterator>::Item> {
-        self.next_token()
-    }
-}
-
-fn read_entire_file<P: AsRef<Path>>(file_path: P) -> Result<String, Box<dyn Error>> {
-    let file = File::open(file_path)?;
-    let er = EventReader::new(file);
-    let mut content = String::new();
-
-    for event in er.into_iter() {
-        if let XmlEvent::Characters(event) = event? {
-            content.push_str(&event);
-        }
-    }
-    Ok(content)
+    write_tf_to_file(tf_index).unwrap_or_else(|err| {
+        eprintln!("{}", err);
+        exit(1);
+    })
 }
