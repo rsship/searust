0f8d7923ae096acb7604564103cf0b0207928b54
bozkayasalihx <jamesgosling10@gmail.com>
2023-09-11 20:22:02 +0300

    improved dir iterator

--- src/main.rs
+++ src/main.rs
@@ -2,89 +2,62 @@ mod config;
 mod lexer;
 mod util;
 
+use clap::Parser;
 use lexer::*;
 use std::fs::{self, DirEntry};
 use std::io;
 use std::path::Path;
 use util::utils::*;
 
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    index: Option<Box<Path>>,
+
+    #[arg(short, long)]
+    search: Option<String>,
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // let config = config::Config::parse().unwrap_or_else(|| {
-    //     exit(1);
-    // });
-
-    let config = config::Config {
-        dir: Path::new("/home/malware/docs.gl/gl4"),
-    };
-
+    let args = Args::parse();
     let mut tf_index = TermFreqIndex::new();
-
     let comput_tf = &mut |entry: &DirEntry| {
         let path = entry.path();
-        let content = read_entire_file(&path)
-            .expect("couldn't read the entire file")
-            .chars()
-            .collect::<Vec<_>>();
-        println!("Indexing {}... ", path.display());
 
-        let mut tf = TermFreq::new();
+        if let Ok(content) = read_entire_file(&path) {
+            let content = content.chars().collect::<Vec<_>>();
+            println!("Indexing {}... ", path.display());
 
-        for token in Lexer::new(&content) {
-            let token = token
-                .into_iter()
-                .map(|x| x.to_ascii_lowercase())
-                .collect::<String>();
+            let mut tf = TermFreq::new();
 
-            if let Some(count) = tf.get_mut(&token) {
-                *count += 1;
-            } else {
-                tf.insert(token, 1);
+            for token in Lexer::new(&content) {
+                let token = token
+                    .into_iter()
+                    .map(|x| x.to_ascii_lowercase())
+                    .collect::<String>();
+
+                if let Some(count) = tf.get_mut(&token) {
+                    *count += 1;
+                } else {
+                    tf.insert(token, 1);
+                }
             }
+
+            let mut stats = tf.iter().collect::<Vec<_>>();
+            stats.sort_by_key(|(_, f)| *f);
+            stats.reverse();
+
+            tf_index.insert(path, tf);
+        } else {
+            println!("unkown format: {:?}", path);
         }
-
-        let mut stats = tf.iter().collect::<Vec<_>>();
-        stats.sort_by_key(|(_, f)| *f);
-        stats.reverse();
-
-        tf_index.insert(path, tf);
     };
 
-    visit_dirs(config.dir, comput_tf)?;
-    // let dir = fs::read_dir(config.file_path)?;
-    //
-    // let mut tf_index = TermFreqIndex::new();
-    //
-    // for entry in dir {
-    //     let mut tf = TermFreq::new();
-    //     let path = entry.expect("TODO:").path();
-    //
-    //     let content = read_entire_file(&path)?.chars().collect::<Vec<_>>();
-    //
-    //     println!("Indexing {}... ", path.display());
-    //
-    //     for token in Lexer::new(&content) {
-    //         let token = token
-    //             .into_iter()
-    //             .map(|x| x.to_ascii_lowercase())
-    //             .collect::<String>();
-    //
-    //         if let Some(count) = tf.get_mut(&token) {
-    //             *count += 1;
-    //         } else {
-    //             tf.insert(token, 1);
-    //         }
-    //     }
-    //
-    //     let mut stats = tf.iter().collect::<Vec<_>>();
-    //     stats.sort_by_key(|(_, f)| *f);
-    //     stats.reverse();
-    //
-    //     tf_index.insert(path, tf);
-    // }
-    //
-    // println!("Indexing Done...");
-    //
-    // write_tf_to_file(tf_index)?;
+    let index = args.index.expect("couldn't find the index location");
+    visit_dirs(&index, comput_tf)?;
+
+    util::utils::write_tf_to_file(tf_index)?;
 
     Ok(())
 }
@@ -96,6 +69,17 @@ fn visit_dirs(dir: &Path, cb: &mut dyn FnMut(&DirEntry)) -> io::Result<()> {
             let path = entry.path();
 
             if path.is_dir() {
+                let first_char = path
+                    .file_name()
+                    .unwrap()
+                    .to_str()
+                    .unwrap()
+                    .chars()
+                    .next()
+                    .unwrap();
+                if first_char == '.' {
+                    continue;
+                }
                 visit_dirs(&path, cb)?;
             } else {
                 cb(&entry);
