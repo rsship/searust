4477c76ee261de053310ce1014bf681c893d2844
bozkayasalihx <bozkayasalih01x@gmail.com>
2023-09-19 18:12:43 +0300

    fix: refactored indexer codebase

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,30 @@
 mod config;
+mod indexer;
 mod lexer;
 mod util;
 
 use config::*;
-use lexer::*;
+use indexer::Model;
 use std::fs::DirEntry;
 use std::path::Path;
 use std::process::exit;
 use tiny_http::{Response, Server};
-use util::utils::*;
 
 fn indexer(args: &Args) -> Result<(), Box<dyn std::error::Error>> {
-    let mut tf_index = TermFreqIndex::new();
-    let tf = &mut |entry: &DirEntry| {
+    let mut model = Model::new();
+
+    util::utils::walk_dir(Path::new(&args.index), &mut |entry: &DirEntry| {
         let path = entry.path();
-        if let Ok(content) = read_entire_file(&path) {
+        if let Ok(content) = util::utils::read_entire_file(&path) {
             let content = content.chars().collect::<Vec<_>>();
-
-            println!("Indexing {} ... ", path.display());
-
-            let mut tf = TermFreq::new();
-
-            for token in Lexer::new(&content) {
-                if let Some(count) = tf.get_mut(&token) {
-                    *count += 1;
-                } else {
-                    tf.insert(token, 1);
-                }
-            }
-
-            let mut stats = tf.iter().collect::<Vec<_>>();
-            stats.sort_by_key(|(_, f)| *f);
-            stats.reverse();
-
-            tf_index.insert(path, tf);
+            println!("Indexing {path} ... ", path = path.display());
+            model.add_doc(path, &content);
         } else {
             println!("unkown format: {:?}", path);
         }
-    };
+    })?;
+    util::utils::save_model(&model, None)?;
 
-    walk_dir(Path::new(&args.index), tf)?;
-
-    write_tf_to_file(tf_index, None)?;
     Ok(())
 }
 
@@ -58,25 +41,9 @@ fn serve(args: &Args) -> Result<(), Box<dyn std::error::Error>> {
                         let mut content = String::new();
                         request.as_reader().read_to_string(&mut content).unwrap();
                         let content = content.chars().collect::<Vec<_>>();
-                        let lexer = Lexer::new(&content);
+                        let model = util::utils::read_from_model(Path::new(&args.serve))?;
+                        let result = model.search_query(&content);
 
-                        let user_tokens = lexer.collect::<Vec<_>>();
-                        let tf_index = read_tf_index(Path::new(&args.serve))?;
-
-                        let mut result = Vec::<(&Path, f32)>::with_capacity(10);
-
-                        for (path, tf) in &tf_index {
-                            let mut rank = 0f32;
-                            for token in &user_tokens {
-                                rank += compute_tf(&tf, &token) * compute_idf(&tf_index, &token);
-                            }
-                            if rank > 0f32 {
-                                result.push((path, rank))
-                            }
-                        }
-
-                        result.sort_by(|(_, rank), (_, rank2)| rank2.partial_cmp(rank).unwrap());
-                        result.truncate(10);
                         let result = serde_json::to_string(&result)?;
 
                         //NOTE: part of the sending response back to client;
