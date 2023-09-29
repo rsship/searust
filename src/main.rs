930bb72a692adaaf23fe487673f063d2e0beac7e
bozkayasalihx <jamesgosling10@gmail.com>
2023-09-29 11:50:07 +0300

    feat: add parallel thread to search and index same time without need to index before start application

--- src/main.rs
+++ src/main.rs
@@ -7,32 +7,9 @@ use config::*;
 use indexer::Model;
 use std::path::Path;
 use std::process::exit;
+use std::sync::{Arc, Mutex};
+use std::thread;
 use tiny_http::{Response, Server};
-
-fn indexer(args: &Args) -> Result<(), Box<dyn std::error::Error>> {
-    let mut model = Model::new();
-    let file_dir = Path::new(&args.index);
-
-    let file_name = file_dir.file_name().unwrap().to_str().unwrap();
-    let file_name = format!("{}.json", file_name);
-    let file_name = Path::new(&file_name);
-
-    let is_exists = util::try_exists(file_name).unwrap();
-
-    if is_exists {
-        //NOTE warm up the cache
-        println!("Loading model from file");
-        model = util::load_model_from_file(file_name)?;
-    }
-
-    model.walk_dir(file_dir)?;
-
-    if let Some(file_name) = file_dir.file_name() {
-        model.save_model(file_name.to_str().unwrap())?;
-    }
-
-    Ok(())
-}
 
 fn serve(args: &Args) -> Result<(), Box<dyn std::error::Error>> {
     let addr = "127.0.0.1:6969";
@@ -47,12 +24,50 @@ fn serve(args: &Args) -> Result<(), Box<dyn std::error::Error>> {
                         let mut content = String::new();
                         request.as_reader().read_to_string(&mut content).unwrap();
                         let content = content.chars().collect::<Vec<_>>();
-                        let model = util::load_model_from_file(Path::new(&args.serve))?;
+                        let json_path = util::str_to_path(&args.serve);
+
+                        let model: Arc<Mutex<Model>>;
+                        let exists = util::try_exists(&json_path)?;
+
+                        if exists {
+                            let m = util::load_model_from_file(&json_path)?;
+                            model = Arc::new(Mutex::new(m));
+                        } else {
+                            model = Arc::new(Mutex::new(Default::default()));
+                        }
+
+                        {
+                            let model = Arc::clone(&model);
+                            let p_path = Path::new(&args.serve).to_path_buf();
+                            thread::spawn(move || {
+                                let mut model = model.lock().unwrap();
+                                match model.walk_dir(&p_path) {
+                                    Err(err) => {
+                                        eprintln!("got an error: {}", err);
+                                    }
+                                    Ok(_) => {
+                                        println!("indexed every dir");
+                                        match model.save_model(&json_path) {
+                                            Ok(()) => {
+                                                println!(
+                                                    "saved to  {path}",
+                                                    path = p_path.display()
+                                                );
+                                            }
+                                            Err(err) => {
+                                                eprintln!("GOT an ERROR: {:?}", err)
+                                            }
+                                        }
+                                    }
+                                }
+                            });
+                        }
+
+                        let model = Arc::clone(&model);
+                        let model = model.lock().unwrap();
                         let result = model.search_query(&content);
-
                         let result = serde_json::to_string(&result)?;
-
-                        //NOTE: part of the sending response back to client;
+                        //NOTE: part of the sendinkg response back to client;
                         let response = Response::from_string(result).with_header(
                             "Access-Control-Allow-Origin: *"
                                 .parse::<tiny_http::Header>()
@@ -76,21 +91,12 @@ fn serve(args: &Args) -> Result<(), Box<dyn std::error::Error>> {
 fn main() {
     let args = Args::parse();
 
-    if !args.index.is_empty() {
-        indexer(&args).unwrap_or_else(|err| {
-            eprintln!("{err}");
-            exit(1);
-        })
-    }
-
     if !args.serve.is_empty() {
         serve(&args).unwrap_or_else(|err| {
             eprintln!("{err}");
             exit(1);
         })
-    }
-
-    if args.serve.is_empty() && args.index.is_empty() {
+    } else {
         Args::usage();
     }
 }
